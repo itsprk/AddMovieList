653ca84f0d627f54bdf3ca80e949e045b000ecc6
itsprk <prk.shine@gmail.com>
2019-10-19 23:38:27 +0530

    Updates

---
+++ precache-manifest.606f970a3ad1cb017796b87cf1a9e00d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84005a4e6946d323711ef3d93ce9ff99",
+    "url": "/AddMovieList/index.html"
+  },
+  {
+    "revision": "a0f2ec1d6b2a7d268cb1",
+    "url": "/AddMovieList/static/css/main.dc30ffc3.chunk.css"
+  },
+  {
+    "revision": "0f8b2b61fa1bb0f9b0e0",
+    "url": "/AddMovieList/static/js/2.559a1f89.chunk.js"
+  },
+  {
+    "revision": "a0f2ec1d6b2a7d268cb1",
+    "url": "/AddMovieList/static/js/main.222337e6.chunk.js"
+  },
+  {
+    "revision": "2eaf08618ccf3d03b14b",
+    "url": "/AddMovieList/static/js/runtime-main.de7b4afe.js"
+  }
+]);
